ce5bb4b14f8f86ae12ce28c0fcf3f03e7b515c7e
eugeneer-code <evgeniy.tsvet@gmail.com>
2024-07-10 13:48:31 +0300

    подсчёт ближайших событий

--- src/AnniversaryCount.jsx
+++ src/AnniversaryCount.jsx
@@ -7,6 +7,24 @@ import CardContent from '@mui/material/CardContent';
 
 function AnniversaryCount() {
 
+  function calcCount() {
+    var count = 0;
+    for(var i=0; i<persons.length; i++){
+      var [DD, MM, YYYY] = persons.at(i).bd.split('.');
+      var dateNow = new Date();
+      var personDate = new Date(dateNow.getFullYear(), parseInt(MM)-1, parseInt(DD));
+      var diffTime = personDate - dateNow;
+      if(diffTime < 0) continue;
+      var diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
+      if(diffDays < 30)  {
+         if((dateNow.getFullYear() - YYYY) % 10 == 0 || (dateNow.getFullYear() - YYYY) % 5 == 0){
+            count = count + 1;
+         }
+      }
+    }
+    return count;
+}
+
   return (
     <div className="base" sx={{backgroundColor:"grey"}}>
       <Card sx={{ width: 240}}>
@@ -15,7 +33,7 @@ function AnniversaryCount() {
            Юбилеи следующие 30 дней
         </Typography>
         <Typography variant={'h3'} color="text.secondary" gutterBottom>
-           {persons.length}
+           {calcCount()}
         </Typography>
         </CardContent>
       </Card>
--- src/BDCount.jsx
+++ src/BDCount.jsx
@@ -7,6 +7,20 @@ import CardContent from '@mui/material/CardContent';
 
 function BDCount() {
 
+  function calcCount() {
+      var count = 0;
+      for(var i=0; i<persons.length; i++){
+        var [DD, MM, YYYY] = persons.at(i).bd.split('.');
+        var dateNow = new Date();
+        var personDate = new Date(dateNow.getFullYear(), parseInt(MM)-1, parseInt(DD));
+        var diffTime = personDate - dateNow;
+        if(diffTime < 0) continue;
+        var diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
+        if(diffDays < 30)  count = count + 1;
+      }
+      return count;
+  }
+
   return (
     <div className="base" sx={{backgroundColor:"grey"}}>
       <Card sx={{ width: 240}}>
@@ -15,7 +29,7 @@ function BDCount() {
            Дни рождения следующие 30 дней
         </Typography>
         <Typography variant={'h3'} color="text.secondary" gutterBottom>
-           {persons.length}
+           {calcCount()}
         </Typography>
         </CardContent>
       </Card>
